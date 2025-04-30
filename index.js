6af44760f14a7b71e4794790b321f6f3a94bc592
RikuYata <groundallow1212@protonmail.com>
2025-04-30 16:48:32 +0900

    Finish working and refactored palindrome method

--- index.js
+++ index.js
@@ -1,23 +1,29 @@
-// exportを記述
-module.exports = Phrase; 
+// ex
+module.exports = Phrase;
 
 // reverse()メソッドを追加してすべてのStringで使えるようにする
-String.prototype.reverse = function() {
-    return Array.from(this).reverse().join("");
+String.prototype.reverse = function () {
+  return Array.from(this).reverse().join("");
+}
+
+// Phraseオブジェクトを定義する
+function Phrase(content) {
+  this.content = content;
+
+  // パリンドロームのテスト用に変換した訳文を返す
+  this.processedContent = function processedContent() {
+    return this.letters().toLowerCase();
   }
-  
-  // Phraseオブジェクトを定義する
-  function Phrase(content) {
-    this.content = content;
-  
-    // パリンドロームのテスト用に変換した訳文を返す
-    this.processedContent = function processedContent() {
-      return this.content.toLowerCase();
-    }
-  
-    // パリンドロームならtrueを、違うならfalseを返す
-    this.palindrome = function palindrome() {
-      return this.processedContent() === this.processedContent().reverse();
-    }
+
+  // 文字だけを返す
+  // 利用例:
+  // new Phrase("Hello, world!").letters() === "Helloworld"
+  this.letters = function letters() {
+    return this.content.match(/[a-z]/gi || []).join("");
   }
-  
+
+  // パリンドロームならtrueを、違うならfalseを返す
+  this.palindrome = function palindrome() {
+    return this.processedContent() === this.processedContent().reverse();
+  }
+}
